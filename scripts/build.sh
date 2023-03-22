062d47dc39cd91d1599ed854765bf006fd373e90
Jayeudypdx <109179392+Jayeudypdx@users.noreply.github.com>
2023-03-21 17:15:49 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,6 @@
 # Source Configs
 source $CONFIG
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
 # Change to the Source Directry
 cd $SYNC_PATH
 
@@ -21,23 +13,7 @@ else
     ccache -M ${CCACHE_SIZE}
 fi
 
-# Send the Telegram Message
-
-echo -e \
-"
-🛠️ CI|TWRP recovery
-
-✔️ The Build has been Triggered!
-
-📱 Device: "${DEVICE}"
-🖥 Build System: "${TWRP_BRANCH}"
-🌲 Logs: <a href=\"https://cirrus-ci.com/build/${CIRRUS_BUILD_ID}\">Here</a>
-" > tg.html
-
-TG_TEXT=$(< tg.html)
-
-telegram_message "${TG_TEXT}"
-echo " "
+# 
 
 # Run the Extra Command
 $EXTRA_CMD
