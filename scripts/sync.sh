e379581c58b0ae2a548f052ddd0d092df8ffe165
Jayeudypdx <109179392+Jayeudypdx@users.noreply.github.com>
2023-03-21 17:18:29 -0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,13 +6,6 @@ source $CONFIG
 # Change to the Home Directory
 cd ~
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
 
 # Clone the Sync Repo
 cd $SYNC_PATH
