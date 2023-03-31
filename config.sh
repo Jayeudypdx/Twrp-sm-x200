9b509035186912f5e5f81f4d19b07139ab9d19a4
Jayeudypdx <109179392+Jayeudypdx@users.noreply.github.com>
2023-03-31 12:07:48 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -19,7 +19,21 @@ export FILENAME="twrp*.img"
 
 
 # Extra Command
-export EXTRA_CMD=""
+export TW_THEME=landscape_hdpi
+export TW_BRIGHTNESS_PATH="/sys/class/backlight/sprd_backlight/brightness"
+export TW_INPUT_BLACKLIST="hbtp_vm"
+export TW_USE_EXTERNAL_STORAGE=true
+export TW_EXCLUDE_TWRPAPP=true
+export TW_INCLUDE_NTFS_3G=true
+export TW_USE_NEW_MINADBD=true
+export TW_USE_TOOLBOX=true
+export TW_HAS_DOWNLOAD_MODE=true
+export TW_NO_REBOOT_BOOTLOADER=true
+export TW_NO_LEGACY_PROPS=true
+export TW_NO_BIND_SYSTEM=true
+export TW_SCREEN_BLANK_ON_BOOT=true
+export TW_DEVICE_VERSION=1
+export TW_INCLUDE_CRYPTO=true
 
 # Not Recommended to Change
 if [ ! -e $HOME/work ]; then
